31cae32ec9258c7c3dfb707e278ecd1ce6421194
Pavel Bandaronak <p_bandaronak@locport.io>
2022-10-16 23:11:39 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirectEncryption = true) {
+    this.isDirectEncryption = isDirectEncryption;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.offset = 65;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    const keyCharCodes = key.toUpperCase().split('').map(c => c.charCodeAt(0));
+    const encryptedChars = [];
+
+    for (let m = 0, k = 0; m < message.length; m++){
+      if (this.alphabet.includes(message[m])){
+        const messageCharCode = message.charCodeAt(m);
+        const encryptedCharCode = this.calcCharCode(messageCharCode + keyCharCodes[k % keyCharCodes.length]);
+        const encryptedChar = String.fromCharCode(encryptedCharCode);
+        encryptedChars.push(encryptedChar);
+        k +=1;
+      } else {
+        encryptedChars.push(message[m]);
+      }
+    }
+
+    return this.isDirectEncryption ? encryptedChars.join('') : encryptedChars.reverse().join('');
   }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    const keyCharCodes = key.toUpperCase().split('').map(c => c.charCodeAt(0));
+    const encryptedChars = [];
+
+    for (let m = 0, k = 0; m < message.length; m++){
+      if (this.alphabet.includes(message[m])){
+        const messageCharCode = message.charCodeAt(m);
+        const encryptedCharCode = this.calcCharCode(messageCharCode - keyCharCodes[k % keyCharCodes.length]);
+        const encryptedChar = String.fromCharCode(encryptedCharCode);
+        encryptedChars.push(encryptedChar);
+        k +=1;
+      } else {
+        encryptedChars.push(message[m]);
+      }
+    }
+
+    return this.isDirectEncryption ? encryptedChars.join('') : encryptedChars.reverse().join('');
+  }
+
+
+  calcCharCode(start) {
+    if (start < 0)
+      start = start + this.alphabet.length;
+
+    return (start % this.alphabet.length) + this.offset;
+  }
+
 }
 
 module.exports = {
