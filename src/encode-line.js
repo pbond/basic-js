3dafaecac0546d1f950258cb20987f7ae908e518
Pavel Bandaronak <p_bandaronak@locport.io>
2022-10-17 09:40:56 +0300

    feat: solve encode-line, get-email-domain

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    let char = { name: str[0], count: 0};
+
+    const charSequence = str.split('').reduce((acc, chr) => {
+        if (chr === char.name) {
+            char.count += 1;
+        } else {
+            acc.push(char);
+            char = { name: chr, count: 1 };
+        }
+        return acc;
+    }, [])
+    charSequence.push(char);
+
+    return charSequence.map(({name, count}) => ((count > 1) ? count + name : name)).join('');
 }
 
 module.exports = {
-  encodeLine
+    encodeLine
 };
