dd1a1cb98de5f731d83afe0485bae424b3c29ea1
Pavel Bandaronak <p_bandaronak@locport.io>
2022-10-17 10:04:43 +0300

    feat: solve mac-address, file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+
+  const existNames = {};
+
+  return names.map(name => {
+    if (!(name in existNames)) {
+      existNames[name] = 1;
+      return name;
+    }
+
+    const nextName = `${name}(${existNames[name]})`;
+    existNames[nextName] = 1;
+    existNames[name] += 1;
+
+    return nextName;
+  });
 }
 
 module.exports = {
